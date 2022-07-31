63653305bee8ace30d03c42d3cdfd8369a1c8260
Nikhil <nikhilrana884@gmail.com>
2022-07-31 23:35:31 +0530

    lights and gridplane testing

--- website/3d-portfolio/main.js
+++ website/3d-portfolio/main.js
@@ -2,12 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 import { AnimationMixer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene,camera);
-}
 
 const scene = new THREE.Scene();
 
@@ -19,15 +15,39 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setX(-10);
 camera.position.setZ(30);
 
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 10, 15 );
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const torusKnot = new THREE.Mesh( geometry, material );
+scene.add( torusKnot );
+
+const pointLight = new THREE.PointLight(0x0ff000);
+pointLight.position.set(10, 11, 11);
+
+const ambientLight = new THREE.AmbientLight(0x0ff000);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add( gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 
+function animate() {
+  requestAnimationFrame(animate);
 
-const geometry = new THREE.SphereGeometry( 15, 32, 16 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00,wirefram: true } );
-const sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
+  
+  torusKnot.rotation.x += 0.005;
+  torusKnot.rotation.y += 0.005;
+  torusKnot.rotation.z += 0.005;
 
+  controls.update();
+
+  renderer.render(scene,camera);
+}
 
 animate()
