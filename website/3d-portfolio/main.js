8e233e438e19b27b9372b3e855cf72c0572e0264
Nikhil <nikhilrana884@gmail.com>
2022-08-17 20:58:49 +0530

    Update main.js

--- website/3d-portfolio/main.js
+++ website/3d-portfolio/main.js
@@ -103,6 +103,39 @@ createCannonConvex(geometry){
   return new CANNON.ConvexPolyhedron(vertices, faces);
 }
 
+
+function createCar() {
+  const car = new THREE.Group();
+  
+  const backWheel = createWheels();
+  backWheel.position.y = 6;
+  backWheel.position.x = -18;
+  car.add(backWheel);
+  
+  const frontWheel = createWheels();
+  frontWheel.position.y = 6;  
+  frontWheel.position.x = 18;
+  car.add(frontWheel);
+
+  const main = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(60, 15, 30),
+    new THREE.MeshLambertMaterial({ color: 0x78b14b })
+  );
+  main.position.y = 12;
+  car.add(main);
+
+  const cabin = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(33, 12, 24),
+    new THREE.MeshLambertMaterial({ color: 0xffffff })
+  );
+  cabin.position.x = -6;
+  cabin.position.y = 25.5;
+  car.add(cabin);
+
+  return car;
+}
+
+
 function animate() {
   requestAnimationFrame(animate);
   
